279ddb2be4932e201a9f4e9cac35288a6eb464b3
Nicolas Bravo <nicobravo1@hotmail.com>
2018-10-08 17:54:37 -0400

    move method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoMethods.java
@@ -1,6 +1,14 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.qualcomm.robotcore.util.Range;
+
 import org.firstinspires.ftc.teamcode.HardwareMap;
 
 public abstract class AutoMethods extends HardwareMap {
+    public void move(double angle, double power) {
+        leftFrontDrive.setPower(Range.clip(power * ((Math.sin((angle + 135) / 180 * Math.PI))), -1, 1));
+        leftBackDrive.setPower(Range.clip(power * ((Math.sin((angle + 45) / 180 * Math.PI))), -1, 1));
+        rightFrontDrive.setPower(Range.clip(power * ((Math.sin((angle + 45) / 180 * Math.PI))), -1, 1));
+        rightBackDrive.setPower(Range.clip(power * ((Math.sin((angle + 135) / 180 * Math.PI))), -1, 1));
+    }
 }
