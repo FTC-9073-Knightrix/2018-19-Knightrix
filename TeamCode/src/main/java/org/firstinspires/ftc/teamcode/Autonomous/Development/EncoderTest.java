fc8e674c970a734e6ce994c193ec5bbf8bbc4a3d
Ignacio Bravo <ibravo@hotmail.com>
2018-12-14 15:29:10 -0500

    Updated EncoderTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Development/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Development/EncoderTest.java
@@ -21,26 +21,13 @@ public class EncoderTest extends AutoMethods {
         waitForStart();
         //mecanumMove("y", 50, 0.3, 0);
 
-        gyroMove(00, 0.3, 20, 1000);
-        //turn(90);
-        sleep(1000);
-
-        gyroMove(180, 0.3, 20, 1000);
-        sleep(1000);
-
-        gyroMove(45, 0.3, 20, 1000);
-        sleep(1000);
-
-        gyroMove(225, 0.3, 20, 1000);
-        sleep(1000);
-
-        gyroMove(315, 0.3, 20, 1000);
-        sleep(1000);
-
         gyroMove(270, 0.3, 20, 1000);
         sleep(1000);
 
-        gyroMove(90, 0.3, 20, 1000);
+        gyroMove(89, 0.3, 20, 1000);
+        sleep(1000);
+
+        gyroMove(91, 0.3, 20, 1000);
         sleep(1000);
 
         gyroMove(135, 0.3, 20, 1000);
